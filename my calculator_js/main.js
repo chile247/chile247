c0ce515d503f364ae18ccc9c51d210e8503e78a0
chile247 <57862853+chile247@users.noreply.github.com>
2022-05-05 00:16:29 +0100

    Add files via upload

---
+++ my calculator_js/main.js
@@ -0,0 +1,21 @@
+const display1E1 = document.querySelector('.display-1');
+const display2E1 = document.querySelector('.display-2');
+const resultDisplayE1 = document.querySelector('.result-display');
+const numberE1 = document.querySelectorAll('.number');
+const operationE1 = document.querySelectorAll('.operation');
+const equalE1 = document.querySelector('.equal');
+const clearE1 = document.querySelector('.all-clear');
+const clearLastE1 = document.querySelector('.clear-lastaction');
+
+
+let dis1Num = '';
+let dis2Num = '';
+let result = null;
+let lastOperation = '';
+let haveFullstop = false;
+
+
+
+
+
+    
